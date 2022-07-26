3cc53e105195fb127b6850a06a13500192fa5884
lee892 <leejoseph892@gmail.com>
2022-07-26 13:34:44 -0500

    uibuild

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(logger('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('build'))
 
 //routes
 app.use('/api/login', loginRouter)
